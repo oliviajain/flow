bfd2d92c59cda8e94d91e7c987c9a1a9d91a92f5
Olivia Jain <oliviajain@berkeley.edu>
2019-08-19 16:52:52 -0700

    Update FlowView.java

--- files/FlowView.java
+++ files/FlowView.java
@@ -7,19 +7,6 @@ import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 public class FlowView
   extends JFrame
 {
@@ -38,66 +25,54 @@ public class FlowView
     JLayeredPane layeredPanel = new JLayeredPane();
     setContentPane(layeredPanel);
     layeredPanel.setBounds(0, 0, 635, 700);
-    
 
     JPanel mainPanel = new JPanel();
     
     mainPanel.setLayout(new BorderLayout());
     mainPanel.setBounds(0, 0, layeredPanel.getWidth(), layeredPanel.getHeight());
-    
 
     flowPanel = new FlowPanel();
     flowPanel.setPreferredSize(new Dimension(635, 635));
     mainPanel.add(flowPanel, "Center");
-    
 
     northPanel = new JPanel();
     northPanel.setLayout(new BorderLayout());
     northPanel.setPreferredSize(new Dimension(635, 30));
-    
 
     secondsRemaining = new JLabel("Seconds Left: ");
     timerLabel = new JLabel("60");
     timerLabel.setFont(new Font("SansSerif", 0, 20));
     timerLabel.setForeground(Color.RED);
-    
-
+   
     JPanel eastNorthPanel = new JPanel();
     eastNorthPanel.add(secondsRemaining);
     eastNorthPanel.add(timerLabel);
     
-
     scoreTextLabel = new JLabel("SCORE: ");
     scoreTextLabel.setFont(new Font("SansSerif", 0, 15));
     scoreLabel = new JLabel("0");
     scoreLabel.setFont(new Font("SansSerif", 0, 20));
     scoreLabel.setForeground(Color.MAGENTA);
-    
 
     JPanel westNorthPanel = new JPanel();
     westNorthPanel.add(scoreTextLabel);
     westNorthPanel.add(scoreLabel);
-    
 
     resignLabel = new JLabel("Click here to RESIGN GAME.");
     resignLabel.setFont(new Font("SansSerif", 0, 20));
     resignLabel.setForeground(Color.black);
     
-
     resignPanel = new JPanel();
     resignPanel.add(resignLabel, "Center");
     
-
     northPanel.add(eastNorthPanel, "East");
     northPanel.add(westNorthPanel, "West");
     northPanel.add(resignPanel);
     
     mainPanel.add(northPanel, "North");
     
-
     topPanel = new FlowTopPanel();
     
-
     layeredPanel.add(mainPanel, new Integer(0), 0);
     layeredPanel.add(topPanel, new Integer(1), 0);
     
